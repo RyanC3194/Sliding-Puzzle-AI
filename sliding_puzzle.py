5ca1886c1fe1c841fe62a79b3313eac4f51ccfeb
Ryan3194C <chen3194@purdue.edu>
2021-03-02 02:07:24 +0800

    Game logic donw

--- sliding_puzzle.py
+++ sliding_puzzle.py
@@ -1,4 +1,6 @@
 import random
+import keyboard
+
 
 
 class Game:
@@ -6,30 +8,82 @@ class Game:
         self.size = size
         number = list(range(1, size ** 2))
         random.shuffle(number)
-        number.append("")
+        number.append(-1)
         self.grid = [number[size * i: size * i + size] for i in range(0, size)]
 
-    def findSpace(self):
+    def find_space(self):
         for i, row in enumerate(self.grid):
             for j, element in enumerate(row):
-                if element == "":
+                if element == -1:
                     return i, j
 
-    def toString(self):
-        print("-" * (self.size * 3 + 1))
+    def on_keypress(self, key):
+        if key == 'w':
+            self.up()
+        if key == 'a':
+            self.left()
+        if key == 's':
+            self.down()
+        if key == 'd':
+            self.right()
+        print(self.toString())
+        if (self.is_complete()):
+            print("Completed")
+            keyboard.remove_hotkey('a')
+            keyboard.add_hotkey('w')
+            keyboard.remove_hotkey('s')
+            keyboard.add_hotkey('d')
+
+    def up(self):
+        i, j = self.find_space()
+        if i != self.size - 1:
+            self.grid[i][j] = self.grid[i + 1][j]
+            self.grid[i + 1][j] = -1
+
+    def down(self):
+        i, j = self.find_space()
+        if i != 0:
+            self.grid[i][j] = self.grid[i - 1][j]
+            self.grid[i - 1][j] = -1
+
+    def right(self):
+        i, j = self.find_space()
+        if j != 0:
+            self.grid[i][j] = self.grid[i][j - 1]
+            self.grid[i][j - 1] = -1
+
+    def left(self):
+        i, j = self.find_space()
+        if j != self.size - 1:
+            self.grid[i][j] = self.grid[i][j + 1]
+            self.grid[i][j + 1] = -1
+
+    def is_complete(self):
+        arr = []
         for row in self.grid:
-            print("|", end="")
+            arr.extend(row)
+        return sorted(arr[:-1]) == arr[:-1]
+
+    def toString(self):
+        out = "-" * (self.size * 3 + 1) + "\n"
+        for row in self.grid:
+            out += "|"
             for element in row:
-                if element == "":
-                    print("  ", end="|")
+                if element == -1:
+                    out += "  |"
                 else:
                     if element / 10 >= 1:
-                        print(element, end="|")
+                        out += str(element) + "|"
                     else:
-                        print(" " + str(element), end="|")
-            print("")
-        print("-" * (self.size * 3 + 1))
+                        out += " " + str(element) + "|"
+            out += "\n"
+        out += "-" * (self.size * 3 + 1) + "\n"
+        return out
 
+    def start(self):
+        print(self.toString())
+        keyboard.add_hotkey('d', self.on_keypress, args='d')
+        keyboard.add_hotkey('a', self.on_keypress, args='a')
+        keyboard.add_hotkey('w', self.on_keypress, args='w')
+        keyboard.add_hotkey('s', self.on_keypress, args='s')
 
-
-print(Game().findSpace())
